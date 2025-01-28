66f28f69932d4dd14765446f78906ded02d3c2d2
mutithegreat <info@isiklarhidrolik.com>
2025-01-28 14:35:09 +0300

    LoginPage added

---
+++ loginPage.py
@@ -0,0 +1,31 @@
+import sqlite3
+from PyQt5.QtWidgets import *
+from loginPageUi import Ui_Form
+
+
+
+class Login(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.loginForm = Ui_Form()
+        self.loginForm.setupUi(self)
+        
+        
+        """self.loginForm.pbGiris.clicked.connect(self.giris)
+        
+
+    def giris(self):
+        self.mainPageOpen.show()
+        kadi = self.loginForm.lineEdit.text()
+        sifre = self.loginForm.lineEdit_2.text()
+        self.con = sqlite3.connect("database.db")
+        self.cursor = self.con.cursor()
+        self.cursor.execute("select * from login where kullanici_adi = ? and kullanici_sifre = ?",(kadi,sifre))
+        data = self.cursor.fetchall()
+        
+        if len(data) > 0:
+            if data[0][0] == kadi and data[0][1] == sifre:
+                    
+                self.hide()
+                self.mainPageOpen.show()
+            """
